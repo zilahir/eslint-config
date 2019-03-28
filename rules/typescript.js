3f6a2336689695ba1c656a022ccf1e34720bc205
Samuel Meuli <me@samuelmeuli.com>
2019-03-28 14:52:50 +0100

    Disable broken TypeScript rules

--- rules/typescript.js
+++ rules/typescript.js
@@ -1,4 +1,8 @@
 module.exports = {
 	// Do not make accessibility modifiers (public/package/private) mandatory
 	"@typescript-eslint/explicit-member-accessibility": "off",
+
+	// TODO Remove the following exceptions once fixed
+	"@typescript-eslint/no-var-requires": "off", // Seems to be broken (also marks normal imports)
+	"import/named": "off", // Seems to be broken for TypeScript (incorrect "not found" errors)
 };
